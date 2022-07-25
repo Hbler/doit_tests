2ac35a1ad0ff9d7a7762d3b4938c263eee875081
Hugo Ernesto <mail.hbler@gmail.com>
2022-07-25 16:49:23 -0300

    test: updated login integration text, invalid credentials scenario

--- src/__tests__/pages/Login.spec.js
+++ src/__tests__/pages/Login.spec.js
@@ -36,4 +36,25 @@ describe("Login Page", () => {
       expect(mockHistoryPush).toHaveBeenCalledWith("/dashboard");
     });
   });
+
+  it("Should not be able to sign in with invalid credentials", async () => {
+    apiMock.onPost("/login").replyOnce(200, {});
+    render(<Login authenticated={false} setAuthenticated={() => {}} />);
+
+    const emailField = screen.getByPlaceholderText("Seu melhor email");
+    const passwordField = screen.getByPlaceholderText("Uma senha bem segura");
+    const buttonElement = screen.getByText("Enviar");
+
+    fireEvent.change(emailField, { target: { value: "johndoe@mail" } });
+    fireEvent.change(passwordField, { target: { value: "123456" } });
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(emailField).toHaveValue("johndoe@mail");
+      expect(passwordField).toHaveValue("123456");
+      expect(screen.getByText("- Email inválido")).toBeInTheDocument();
+      expect(screen.getByText("- Mínimo de 8 dígitos")).toBeInTheDocument();
+      expect(mockHistoryPush).not.toHaveBeenCalledWith("/dashboard");
+    });
+  });
 });
